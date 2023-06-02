8cb950d63184598efa6c9f5d5bfd7fa1acdbadcc
BurhanRaja <burhanraja02@yahoo.com>
2023-06-02 20:02:29 +0530

    assets, components, contants and style added

--- app/index.js
+++ app/index.js
@@ -1,12 +1,21 @@
-import {View, Text} from "react-native";
-import { Link, Stack } from "expo-router";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Link, Stack, useRouter } from "expo-router";
+import { useState } from "react";
+
+import { COLORS, images, SIZES, icons } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 export default function Home() {
   return (
     <View style={{}}>
-        <Text>Home</Text>
-        <Stack.Screen options={{ title: "Overview" }} />
-        <Link href="/details">Go to Details</Link>
+      <Text>Home</Text>
+      <Stack.Screen options={{ title: "Overview" }} />
+      <Link href="/details">Go to Details</Link>
     </View>
-  )
+  );
 }
